fd869fbc394025a13ae40b507763e93e4e6e1c2c
Rayon <rayon@kali.local>
2021-05-02 07:43:19 +0545

    notifications completed

--- posts/context_processors.py
+++ posts/context_processors.py
@@ -2,7 +2,8 @@ from posts.models import Notification
 def show_notifications(request):
 	request_user = request.user
 	if request_user.is_authenticated:
-		notifications = Notification.objects.filter(to_user=request_user).exclude(user_has_seen=True).order_by('-date')
-		return {'notifications': notifications}
+		notifications_one = Notification.objects.filter(to_user=request_user).exclude(user_has_seen=True).order_by('-date')
+		notifications_two = notifications_one.filter(to_user=request_user).exclude(from_user = request_user).order_by('-date')
+		return {'notifications': notifications_two}
 	else:
 		return {'nice':'to meet you'}
